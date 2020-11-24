caefc8c4bc35f00f2b37683a313db256ebbb4e29
midbel <brun.nicolas21@gmail.com>
2020-11-24 10:29:52 +0100

    show details of selected log files

---
+++ src/components/Info.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="col-md-12 col-lg-12 d-md-block bg-light px-3 py-2 border-top bg-light">
+    <dl class="row" v-if="filename">
+      <dt class="col-4">file</dt>
+      <dd class="col-8 text-right">{{filename}}</dd>
+      <dt class="col-4">size</dt>
+      <dd class="col-8 text-right">{{size}}</dd>
+      <dt class="col-4">modtime</dt>
+      <dd class="col-8 text-right">{{modtime}}</dd>
+    </dl>
+    <p v-else class="text-center text-muted my-4">no log selected</p>
+  </div>
+</template>
+
+<script>
+import { DateTime } from 'luxon'
+
+const kilo = 1024
+const mega = kilo*kilo
+const giga = mega*kilo
+const tera = giga*kilo
+
+export default {
+  name: 'Info',
+  computed: {
+    size() {
+      let z = this.$store.state.info.size
+      if (z < kilo) {
+        return z
+      } else if (z < mega) {
+        return `${z >> 10}KB`
+      } else if (z < giga) {
+        return `${z >> 20}MB`
+      } else if (z < tera) {
+        return `${z >> 30}GB`
+      } else {
+        return `${z >> 40}TB`
+      }
+    },
+    filename() {
+      return this.$store.state.info.file
+    },
+    modtime() {
+      let when = DateTime.fromISO(this.$store.state.info.modtime)
+      return when.toLocaleString(DateTime.DATETIME_SHORT)
+    }
+  },
+}
+</script>
+
+<style scoped>
+div {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  height: 120px;
+}
+
+dl>dt {
+  font-size: 13px;
+}
+dl>dd {
+  font-size: 14px;
+}
+</style>
